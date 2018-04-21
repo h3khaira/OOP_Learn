abb6e3c129f719467f15350b90fd99b9f686d660
unknown <h3khaira@edu.uwaterloo.ca>
2018-04-20 20:36:27 -0400

    Harseerat

--- Snake.py
+++ Snake.py
@@ -1,5 +1,6 @@
 import pygame
-#pygame.init()
+import random
+pygame.init()
 pygame.font.init()
 #length will be in units,status will be boolean alive or dead,headx and heady will dictate position 
 class snake:
@@ -8,18 +9,26 @@ class snake:
         snake.status=status
         snake.headx=headx
         snake.heady=heady
+    def drawsnake()
+        
 class food:
-    def __init__(self,locx,locy):
+    def __init__(self,locx,locy,width,height):
         food.locx=locx
         food.locy=locy
+        food.width=width
+        food.height=height
+    def drawfood()
+
         
 def main():
     screen_width=600
     screen_height=600
-    pygame.display.set_mode([screen_width,screen_height])
-    pygame.display.flip()
+    screen=pygame.display.set_mode([screen_width,screen_height])
     while True:
         pass
 
+if __name__ == '__main__':
+    main()
+   
 
 
