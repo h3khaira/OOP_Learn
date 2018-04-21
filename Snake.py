c17ec04d9f9a79f146ac46b2de9a04aff7422eb4
unknown <h3khaira@edu.uwaterloo.ca>
2018-04-20 21:42:20 -0400

    Harseerat

--- Snake.py
+++ Snake.py
@@ -2,16 +2,16 @@ import pygame
 from random import randint
 pygame.init()
 pygame.font.init()
-#length will be in units,status will be boolean alive or dead,headx and heady will dictate position 
+#length will be in units,headx and heady will dictate position 
 class snake:
-    def __init__(self,length,status,headx,heady):
+    def __init__(self,length,segments,headx,heady):
         self._length=length
-        self._status=status
         self._headx=headx
         self._heady=heady
     def drawsnake(self):
-        
+        pygame.draw.rect(self._width,self._height,self.locx,self.locy)
     def movesnake(self,xspeed,yspeed):
+         move_ip()
         
 class food:
     def __init__(self,locx,locy,width,height):
@@ -19,26 +19,43 @@ class food:
         self._locy=locy
         self._width=width
         self._height=height
-    def drawfood()
-        self.locx=randint(1,screen_width)
-        self.locy=randint(1,screen_height)
-        pygame.draw.rect(self._width,self._height,self.locx,self.locy)
+    #def drawfood()
+     #   self.locx=randint(1,screen_width)
+      #  self.locy=randint(1,screen_height)
+       # pygame.draw.rect(self._width,self._height,self.locx,self.locy)
 
         
 def main():
+    white=(255,255,255)
+    black=(0,0,0)
     screen_width=600
     screen_height=600
     screen=pygame.display.set_mode([screen_width,screen_height])
+    pygame.display.set_caption('Snake')
+    
+    screen.fill(white)
+    pygame.display.update()
+    done=False
     snakex=300
     snakey=300
-    while True:
-
+    key=pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        snake.movesnake(-1,0)
+    if key[pygame.K_RIGHT]:
+        snake.movesnake(1, 0)
+    if key[pygame.K_UP]:
+        snake.movesnake(0, 1)
+    if key[pygame.K_DOWN]:
+        snake.movesnake(0, -1)
+    while not done:
+        True
+        #snake.drawsnake()
 
         
 
 if __name__ == '__main__':
     main()
-
+    pygame.quit()
    
 
 
